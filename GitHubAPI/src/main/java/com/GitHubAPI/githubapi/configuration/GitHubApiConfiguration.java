cb3cc00116393c21fb180fd7159220459032bc49
ranierig <ranierig@gmail.com>
2025-07-17 08:23:49 -0300

    Atribuindo variável em arquivo para a url

---
+++ GitHubAPI/src/main/java/com/GitHubAPI/githubapi/configuration/GitHubApiConfiguration.java
@@ -0,0 +1,13 @@
+package com.githubapi.githubapi.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class GitHubApiConfiguration {
+    @Bean
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder.baseUrl("https://api.github.com").build();
+    }
+}
